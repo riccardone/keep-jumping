d632b392dc7beb6ed094f68b5bb364d28076224e
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2022-02-19 18:57:44 +0000

    publish a death event when the player fall

---
+++ Assets/Game/Scripts/LoadSceneButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Game.Scripts
+{
+    public class LoadSceneButton : MonoBehaviour
+    {
+        public string SceneName = "";
+
+        void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject == gameObject
+                && Input.GetButtonDown(GameConstants.k_ButtonNameSubmit))
+            {
+                LoadTargetScene();
+            }
+        }
+
+        public void LoadTargetScene()
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
