63e1df3302dd92244e1862a01c24b6fc083813f4
Vincent C <87056334+SisyphusStoned@users.noreply.github.com>
2022-02-19 11:04:18 -0600

    Timer and Timer Assets
    Added Timer Script and Timer assets, yet to link any object on scene

---
+++ Assets/Game/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    private float remainingTime;
+    private bool timerIsRunning = false;
+    public Text OSTimer;
+    [Header("Timings")]
+    [Tooltip("Game time in seconds")]
+    public float maxGameTime; //total game time to start countdown
+
+    //Display Time 
+    void timeDisplay()
+    {
+        float minute = Mathf.FloorToInt(remainingTime/60);
+        float second = Mathf.Floor(remainingTime%60);
+        OSTimer.text=string.Format("{0:00}:{1:00}",minute,second);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainingTime = maxGameTime;
+        timerIsRunning = true;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            //update timer if time not finished
+            if(remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            //Do whatever is needed when time is out
+            else
+            {
+                Debug.Log("Time ran out");
+                timerIsRunning = false;
+                remainingTime = 0;
+                SceneManager.LoadScene("Time Out");
+            }  
+        }
+    }
+}
