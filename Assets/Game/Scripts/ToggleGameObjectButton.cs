b99d88de52eb42f48553aa568aa8d95106b9d72c
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2022-02-20 11:19:53 +0000

    add intro screen

---
+++ Assets/Game/Scripts/ToggleGameObjectButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.Scripts
+{
+    public class ToggleGameObjectButton : MonoBehaviour
+    {
+        public GameObject ObjectToToggle;
+        public bool ResetSelectionAfterClick;
+
+        void Update()
+        {
+            if (ObjectToToggle.activeSelf && Input.GetButtonDown(GameConstants.k_ButtonNameCancel))
+            {
+                SetGameObjectActive(false);
+            }
+        }
+
+        public void SetGameObjectActive(bool active)
+        {
+            ObjectToToggle.SetActive(active);
+
+            if (ResetSelectionAfterClick)
+                EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+}
